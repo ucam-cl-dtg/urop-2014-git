f3088a996ea597186b2098075368843557aeaac3
gitolite tester <tester@example.com>
2014-07-02 13:30:11 +0100

    Written first implementation of generateConfigFile, including successfully calling hook. Works only for me though.

--- src/main/java/uk/ac/cam/UROP/twentyfourteen/ConfigDatabase.java
+++ src/main/java/uk/ac/cam/UROP/twentyfourteen/ConfigDatabase.java
@@ -2,6 +2,13 @@
 /* See the LICENSE file for the license of the project */
 package uk.ac.cam.UROP.twentyfourteen;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Isaac Dunn <ird28@cam.ac.uk>
  * @author Kovacsics Robert <rmk35@cam.ac.uk>
@@ -16,13 +23,42 @@ public class ConfigDatabase {
 	 * 
 	 * @return The gitolite config file
 	 */
-	public void generateConfigFile() {
+	public static void generateConfigFile() {
 		/* TODO: implement
          *
          * 1) Create a new StringBuilder
          * 2) Fill in StringBuilder according to a template
          * 3) Write file to disk
          */
+		StringBuilder output = new StringBuilder();
+		output.append("repo create-new\n");
+		output.append("    RW = admin\n");
+		output.append("    R  = u2");
+		try {
+			File configFile = new File("/home/ird28/test.conf");
+			BufferedWriter buffWriter = new BufferedWriter(new FileWriter(configFile, false));
+			buffWriter.write(output.toString());
+			buffWriter.close();
+			Process p = Runtime.getRuntime().exec("/home/ird28/.gitolite/hooks/gitolite-admin/post-update",
+						new String[] {"HOME=/home/ird28", "PATH=/home/ird28/bin/:/bin:/usr/bin", "GL_LIBDIR=/home/ird28/git/gitolite/src/lib"});
+			
+			BufferedReader errorReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			BufferedReader outputReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line;
+			while ((line = outputReader.readLine()) != null) {
+				System.out.println(line);
+			}
+			while ((line = errorReader.readLine()) != null) {
+				System.err.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		generateConfigFile();
+		System.out.println("Done");
 	}
 	
 	
