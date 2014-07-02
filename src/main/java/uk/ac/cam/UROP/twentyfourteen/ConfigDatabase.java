c55637b293ab23f6eb438e8ce9d63877e58244a3
Isaac Dunn <ird28@cam.ac.uk>
2014-07-02 15:42:19 +0100

    generateConfigFile now reads repo names from mongoDB

--- src/main/java/uk/ac/cam/UROP/twentyfourteen/ConfigDatabase.java
+++ src/main/java/uk/ac/cam/UROP/twentyfourteen/ConfigDatabase.java
@@ -8,6 +8,14 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.Cursor;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+
+import uk.ac.cam.UROP.twentyfourteen.database.Mongo;
 
 /**
  * @author Isaac Dunn <ird28@cam.ac.uk>
@@ -31,9 +39,13 @@ public class ConfigDatabase {
          * 3) Write file to disk
          */
 		StringBuilder output = new StringBuilder();
-		output.append("repo create-new\n");
-		output.append("    RW = admin\n");
-		output.append("    R  = u2");
+		Cursor allRepos = Mongo.getDB().getCollection("repos").find();
+		while (allRepos.hasNext()) {
+			//DBObject repoDoc = allRepos.next();
+			output.append("repo " + allRepos.next().get("repoName") + "\n");
+			output.append("    RW = admin\n");
+			output.append("    R  = u1\n\n");
+		}
 		try {
 			String home = System.getProperty("user.home");
 			File configFile = new File(home+"/test.conf");
@@ -42,8 +54,7 @@ public class ConfigDatabase {
 			buffWriter.close();
 			Process p = Runtime.getRuntime().exec(home+"/.gitolite/hooks/gitolite-admin/post-update",
 						new String[] {"HOME="+home, "PATH="+home+"/bin/:/bin:/usr/bin", "GL_LIBDIR="+home+"/git/gitolite/src/lib"});
-			
-			BufferedReader errorReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+						BufferedReader errorReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 			BufferedReader outputReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String line;
 			while ((line = outputReader.readLine()) != null) {
@@ -57,7 +68,19 @@ public class ConfigDatabase {
 		}
 	}
 	
+	public static void addRepo(String repoName) {
+		DBCollection repoTable = Mongo.getDB().getCollection("repos");
+		BasicDBObject repoDoc = new BasicDBObject();
+		repoDoc.put("repoName", repoName);
+		repoTable.insert(repoDoc);
+	}
+	
 	public static void main(String[] args) {
+		DBCollection repoTable = Mongo.getDB().getCollection("repos");
+		if (repoTable != null)
+			repoTable.remove(new BasicDBObject());
+		addRepo("test-repo-one");
+		addRepo("test-repo-two");
 		generateConfigFile();
 		System.out.println("Done");
 	}
