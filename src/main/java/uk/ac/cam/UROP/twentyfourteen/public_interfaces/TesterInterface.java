901466a0a3b90725c136e31bcc9c8625681169ef
Kovacsics Robert (Arch) <kovirobi@gmail.com>
2014-07-02 10:23:48 +0100

    Feedback-required interface for testing end

---
+++ src/main/java/uk/ac/cam/UROP/twentyfourteen/public_interfaces/TesterInterface.java
@@ -0,0 +1,39 @@
+/* vim: set et ts=4 sts=4 sw=4 tw=72 : */
+/* See the LICENSE file for the license of the project */
+package uk.ac.cam.UROP.twentyfourteen.public_interfaces;
+
+import java.util.Map;
+import java.util.Collection;
+
+/**
+ * This is the interface the testing end of the project should use.
+ * <p>
+ * For a concrete implementation
+ * @author Isaac Dunn <ird28@cam.ac.uk>
+ * @author Kovacsics Robert <rmk35@cam.ac.uk>
+ * @version 0.1
+ */
+public interface TesterInterface
+{
+    /**
+     * Gets the source files
+     *
+     * @return List of source files
+     */
+    public Collection<String> getSources();
+
+    /**
+     * Returns a map of test files and a list of required files for
+     * those tests to run
+     *
+     * @return Map of test files and a list of the test's dependencies
+     */
+    public Map<String, Collection<String>> getTests();
+
+    /**
+     * Callback to let the repository store the test result
+     *
+     * @param
+     */
+    public void saveTestResults(Object results);
+}
