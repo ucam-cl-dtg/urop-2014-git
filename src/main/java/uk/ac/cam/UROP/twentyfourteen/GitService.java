50872b06adae1101b8f3aa76848c9a9868d72c50
Kovacsics Robert (Arch) <kovirobi@gmail.com>
2014-07-10 17:26:56 +0100

    Path parameter added for GitService.

--- src/main/java/uk/ac/cam/UROP/twentyfourteen/GitService.java
+++ src/main/java/uk/ac/cam/UROP/twentyfourteen/GitService.java
@@ -1,9 +1,12 @@
+/* vim: set et ts=4 sts=4 sw=4 tw=72 : */
+/* See the LICENSE file for the license of the project */
 package uk.ac.cam.UROP.twentyfourteen;
 
 import java.util.List;
 import java.util.LinkedList;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
@@ -21,4 +24,17 @@ public class GitService {
 	    return Response.status(200).entity(toReturn.toString()).build();
 	}
 
+    @GET
+    @Path("/git/{repoName}/{pathParam:.*}")
+    public Response getFile(@PathParam("pathParam") String filePath
+                          , @PathParam("repoName")  String repoName)
+    {
+        /* TODO
+         * 1. Open the file given by filePath in the Git repository
+         *    repoName. This can be done with GitDb and TreeWalker
+         *    somehow, to prevent the need of cloning the repository.
+         * 2. Give the contents of the file as the entity
+         */
+        return Response.status(200).entity(filePath).build();
+    }
 }
