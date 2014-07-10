bb6ddbf51a0e269e583d5d9ce22879eb57e99fc6
Isaac Dunn <ird28@cam.ac.uk>
2014-07-10 12:04:09 +0100

    The Guice example now works; the project is now a web project.

---
+++ src/main/java/uk/ac/cam/UROP/twentyfourteen/mongojack/GuiceExperiment.java
@@ -0,0 +1,54 @@
+/* vim: set et ts=4 sts=4 sw=4 tw=72 : */
+/* See the LICENSE file for the license of the project */
+/**
+ * 
+ */
+package uk.ac.cam.UROP.twentyfourteen.mongojack;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+/**
+ * @author ird28
+ *
+ */
+public class GuiceExperiment {
+    
+    private final DB db;
+    private final Writer writer;
+    
+    @Inject
+    public GuiceExperiment(DB db, Writer writer) {
+        this.db = db;
+        this.writer = writer;
+    }
+    
+    public void readThenProcessThenWrite() throws IOException {
+        DBCollection collection = db.getCollection("guicetest");
+        DBCursor results = collection.find();
+        while (results.hasNext()) {
+            DBObject result = results.next();
+            System.out.println((String) result.get("name"));
+            writer.write((String) result.get("name"));
+        }
+        writer.flush();
+        writer.close();
+        System.out.println("done");
+    }
+    
+    public static void main(String[] args) throws IOException {
+        Injector injector = Guice.createInjector(new GuiceModule());
+        GuiceExperiment gex = injector.getInstance(GuiceExperiment.class);
+        gex.readThenProcessThenWrite();
+    }
+    
+
+}
