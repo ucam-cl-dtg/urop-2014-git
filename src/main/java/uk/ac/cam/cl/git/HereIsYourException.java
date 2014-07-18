ae2585d04b9e51ca40df465c86aa6d87f10abbe2
Kovacsics Robert (Arch) <kovirobi@gmail.com>
2014-07-18 14:38:16 +0100

    Unique serialVersionUID for HereIsY... and DirectoryExp...

--- src/main/java/uk/ac/cam/cl/git/HereIsYourException.java
+++ src/main/java/uk/ac/cam/cl/git/HereIsYourException.java
@@ -2,7 +2,7 @@ package uk.ac.cam.cl.git;
 
 public class HereIsYourException extends Exception {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
     
     public HereIsYourException() {
         super("Well you did ask for it...");
