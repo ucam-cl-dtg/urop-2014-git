cd83467ec1e72291597764aaa89d22670fb2f5b8
Kovacsics Robert (Arch) <kovirobi@gmail.com>
2014-07-18 09:45:18 +0100

    Annotated with both Jackson 1 and 2 annotations

--- src/main/java/uk/ac/cam/cl/git/ForkRequestBean.java
+++ src/main/java/uk/ac/cam/cl/git/ForkRequestBean.java
@@ -13,17 +13,20 @@ import uk.ac.cam.cl.git.public_interfaces.ForkRequestInterface;
  * @author Kovacsics Robert &lt;rmk35@cam.ac.uk&gt;
  */
 public class ForkRequestBean implements ForkRequestInterface
-{
+{ /* TODO: Figure out what is causing import of Jackson 1, only use
+   * Jackson 1 or Jackson 2 but not both!
+   */
     private final String newRepoName;
     private final String repoOwner;
     private final String repoName;
     private final String overlay;
 
+    @org.codehaus.jackson.annotate.JsonCreator
     @JsonCreator
-    public ForkRequestBean(@JsonProperty("newRepoName") String newRepoName
-                         , @JsonProperty("repoOwner")   String repoOwner
-                         , @JsonProperty("repoName")    String repoName
-                         , @JsonProperty("overlay")     String overlay)
+    public ForkRequestBean(@org.codehaus.jackson.annotate.JsonProperty("newRepoName") @JsonProperty("newRepoName") String newRepoName
+                         , @org.codehaus.jackson.annotate.JsonProperty("repoOwner")   @JsonProperty("repoOwner")   String repoOwner
+                         , @org.codehaus.jackson.annotate.JsonProperty("repoName")    @JsonProperty("repoName")    String repoName
+                         , @org.codehaus.jackson.annotate.JsonProperty("overlay")     @JsonProperty("overlay")     String overlay)
     {
         if (newRepoName == null)
             this.newRepoName = repoOwner + "/" + repoName;
@@ -35,18 +38,22 @@ public class ForkRequestBean implements ForkRequestInterface
     }
 
     @Override
+    @org.codehaus.jackson.annotate.JsonProperty("newRepoName")
     @JsonProperty("newRepoName")
     public String getNewRepoName() { return newRepoName; }
 
     @Override
+    @org.codehaus.jackson.annotate.JsonProperty("repoOwner")
     @JsonProperty("repoOwner")
     public String getRepoOwner() { return repoOwner; }
     
     @Override
+    @org.codehaus.jackson.annotate.JsonProperty("repoName")
     @JsonProperty("repoName")
     public String getRepoName() { return repoName; }
 
     @Override
+    @org.codehaus.jackson.annotate.JsonProperty("overlay")
     @JsonProperty("overlay")
     public String getOverlay() { return overlay; }
 }
